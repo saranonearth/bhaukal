2c2bea68af1e87dcd71a1b5a4c171c1492cbaef7
saranonearth <saran.suresh@outlook.com>
2019-03-24 01:42:10 +0530

    Updates

---
+++ precache-manifest.60058e078ec5be46c042acab339989d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3098accce645ab062b74c1e29394360c",
+    "url": "/bhaukal/static/media/mbutton.3098accc.svg"
+  },
+  {
+    "revision": "f5bf1e69d9f5c5cc56e41cdaa12de303",
+    "url": "/bhaukal/static/media/instagram.f5bf1e69.png"
+  },
+  {
+    "revision": "7af0d76c8a0952f1aaad",
+    "url": "/bhaukal/static/js/runtime~main.b26ec618.js"
+  },
+  {
+    "revision": "0f28dae974c60266cc2c",
+    "url": "/bhaukal/static/js/main.8618eda9.chunk.js"
+  },
+  {
+    "revision": "ad68d4b2fa6fe3b1b1a8",
+    "url": "/bhaukal/static/js/2.bb2fabbe.chunk.js"
+  },
+  {
+    "revision": "0f28dae974c60266cc2c",
+    "url": "/bhaukal/static/css/main.ffef4f6b.chunk.css"
+  },
+  {
+    "revision": "ad68d4b2fa6fe3b1b1a8",
+    "url": "/bhaukal/static/css/2.95a8f255.chunk.css"
+  },
+  {
+    "revision": "5298d26299f710b6a229e8c54f88ac30",
+    "url": "/bhaukal/index.html"
+  }
+];
